97281e96c0a55c0007e91921cad7a079d2622fce
devaldous <devaldous@gmail.com>
2024-09-19 21:30:47 +0800

    Updated error page

--- src/components/AppCarousel.js
+++ src/components/AppCarousel.js
@@ -2,29 +2,49 @@ import Carousel from "react-bootstrap/Carousel";
 import Image from "react-bootstrap/Image";
 
 function AppCarousel() {
+  const carouselData = [
+    {
+      header: "Own the Streets",
+      description:
+        "Bold, Filipino-inspired streetwear that lets you stand out and repyour roots.",
+      image: "/CarouselImages/carousel-1.jpg",
+    },
+    {
+      header: "Manila Vibes, Global Style",
+      description:
+        "Authentic streetwear inspired by Manila’s culture, wherever you are.",
+      image: "/CarouselImages/carousel-2.jpg",
+    },
+    {
+      header: "Filipino Pride, Street Cool",
+      description:
+        "Street style rooted in Filipino tradition. Fearless, creative, and proud.",
+      image: "/CarouselImages/carousel-3.jpg",
+    },
+    ,
+  ];
+
   return (
     <Carousel>
       <Carousel.Item interval={5000}>
-        <Image src="/CarouselImages/carousel-1.jpg" fluid />
+        <Image src={carouselData[0].image} fluid />
         <Carousel.Caption>
-          <h1>First slide label</h1>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <h1>{carouselData[0].header}</h1>
+          <p>{carouselData[0].description}</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={5000}>
-        <Image src="/CarouselImages/carousel-2.jpg" fluid />
+        <Image src={carouselData[1].image} fluid />
         <Carousel.Caption>
-          <h1>Second slide label</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <h1>{carouselData[1].header}</h1>
+          <p>{carouselData[1].description}</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item interval={5000}>
-        <Image src="/CarouselImages/carousel-3.jpg" fluid />
+        <Image src={carouselData[2].image} fluid />
         <Carousel.Caption>
-          <h1>Third slide label</h1>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          <h1>{carouselData[2].header}</h1>
+          <p>{carouselData[2].description}</p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
--- src/components/AppNavbar.js
+++ src/components/AppNavbar.js
@@ -2,12 +2,14 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { FaRegUserCircle } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 
 function NavigationBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">
+        <Navbar.Brand as={NavLink} to="/">
           <img
             className="d-inline-block align-top"
             src="/Logo/estilo-manila.png"
@@ -17,18 +19,23 @@ function NavigationBar() {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Products</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
+          <Nav className="w-100">
+            <Nav.Link as={NavLink} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/products">
+              Products
+            </Nav.Link>
+            <NavDropdown
+              title={<FaRegUserCircle id="user-icon" />}
+              className="justify-content-end ms-auto"
+              id="basic-nav-dropdown "
+            >
+              <NavDropdown.Item as={NavLink} to="/login">
+                Login
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
+              <NavDropdown.Item as={NavLink} to="/register">
+                Register
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>
