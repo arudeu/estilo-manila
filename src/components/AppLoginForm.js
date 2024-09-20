c58aa7586c558b7f49d8ec7c678f456026e9e2a7
devaldous <devaldous@gmail.com>
2024-09-20 22:22:16 +0800

    Updated Login Page

--- src/components/AppLoginForm.js
+++ src/components/AppLoginForm.js
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
-import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log('Login attempted with:', { email, password });
+    console.log("Login attempted with:", { email, password });
     // Here you would typically send a request to your server
   };
 
   return (
-    <Container>
-      <Row className="justify-content-md-center mt-5">
+    <Container className="my-5 py-5">
+      <Row className="justify-content-md-center py-5">
         <Col xs={12} md={6}>
-          <h2 className="text-center mb-4">Login</h2>
+          <h1 className="text-center mb-4 fw-bolder">Login</h1>
           <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email:</Form.Label>
@@ -39,12 +39,14 @@ function Login() {
               />
             </Form.Group>
             <div className="text-center">
-            <Button type="submit" className="w-20 btn-dark">
-              Login
-            </Button>
+              <Button type="submit" className="w-20 btn-dark">
+                Login
+              </Button>
             </div>
           </Form>
-          <p className="text-center mt-4">Don't have an account yet? <a href="">Click here</a> to register.</p>
+          <p className="text-center mt-4">
+            Don't have an account yet? <a href="">Click here</a> to register.
+          </p>
         </Col>
       </Row>
     </Container>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,17 @@
-import AppLoginForm from "../components/AppLoginForm"
+import AppLoginForm from "../components/AppLoginForm";
 import { Container, Row, Col } from "react-bootstrap";
+import { motion } from "framer-motion";
 
 export default function Login() {
   return (
     <>
-      <AppLoginForm />
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        <AppLoginForm />
+      </motion.div>
     </>
   );
 }
