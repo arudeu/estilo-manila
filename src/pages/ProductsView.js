2d13e911295abf70d64643b835d340fa9e6d056b
devaldous <devaldous@gmail.com>
2024-09-21 16:48:17 +0800

    Added add to cart button

--- src/pages/ProductsView.js
+++ src/pages/ProductsView.js
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { motion } from "framer-motion";
 
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 
 export default function ProductsView() {
@@ -25,26 +26,35 @@ export default function ProductsView() {
   });
 
   return (
-    <Container className="my-5">
-      <Row>
-        <Col className="mb-4" md={4} lg={6}>
-          <Card
-            className="card card-product rounded-0"
-            style={{
-              backgroundImage: `linear-gradient(to bottom, transparent, #000), url(${image})`,
-            }}
-          >
-            <h4 className="fw-bold text-white bottom-0 position-absolute mx-3">
-              ESTILO <span className="fs-6 ">MNL</span>
-            </h4>
-          </Card>
-        </Col>
-        <Col>
-          <h1 className="fw-bolder">{name}</h1>
-          <h2>&#x20B1;{price}</h2>
-          <p>{description}</p>
-        </Col>
-      </Row>
-    </Container>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <Container className="my-5">
+        <Row>
+          <Col className="mb-4" md={4} lg={6}>
+            <Card
+              className="card card-product rounded-0"
+              style={{
+                backgroundImage: `linear-gradient(to bottom, transparent, #000), url(${image})`,
+              }}
+            >
+              <h4 className="fw-bold text-white bottom-0 position-absolute mx-3">
+                ESTILO <span className="fs-6 ">MNL</span>
+              </h4>
+            </Card>
+          </Col>
+          <Col className="product-details mb-4 p-4 position-relative">
+            <h1 className="fw-bolder">{name}</h1>
+            <h2>&#x20B1;{price}</h2>
+            <p>{description}</p>
+            <Button className="btn btn-dark position-absolute add-to-cart">
+              Add To Cart
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </motion.div>
   );
 }
